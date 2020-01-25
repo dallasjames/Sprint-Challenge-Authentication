f615142bbfc2884bf8faf1bfdd43ed6e00b5556a
Dallas James <dallasjames42@gmail.com>
2020-01-24 18:21:04 -0700

    mostly done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,55 @@
-const router = require('express').Router();
+const bcrypt = require("bcryptjs")
+const express = require("express")
+const userModel = require("../users/user-model")
+const jwt = require("jsonwebtoken")
+const secrets = require("../config/secrets")
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+const router = express.Router()
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+router.post("/register", async (req, res, next) => {
+    try {
+        const saved = await userModel.add(req.body)
 
-module.exports = router;
+        res.status(201).json(saved)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post("/login", async (req, res, next) => {
+    try {
+        const { username, password } = req.body
+        const user = await userModel.findBy({ username }).first()
+        const valid = await bcrypt.compare(password, user.password)
+
+        if (user && valid) {
+            const token = generateToken(user)
+            req.session = user
+            res.status(200).json({
+                message: `welcome ${user.username}`,
+                token
+            })
+        } else {
+            res.status(401).json({
+                message: "please dont brute force me"
+            })
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username
+    }
+    const secret = secrets.jwtSecret
+    const options = {
+        expiresIn: "8h"
+    }
+
+    return jwt.sign(payload, secret, options)
+}
+
+module.exports = router
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,12 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const userModel = require("../users/user-model")
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+module.exports = () => {
+  return (req, res, next) => {
+      if (!req.session || !req.session.user) {
+          return res.status(403).json({
+            message: "you shall not pass"
+          })
+      }
+      next()
+  }
+}
